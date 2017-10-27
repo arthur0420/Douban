2e5eae3c5007ba0f1cb9fc5b3bfd9de65ed86ba9
arthur0420 <arthur0420@aliyun.com>
2017-10-27 17:14:56 +0800

    socket

--- src/arthur/douban/queue/mq/Consumer.java
+++ src/arthur/douban/queue/mq/Consumer.java
@@ -1,93 +1,45 @@
 package arthur.douban.queue.mq;
 
-import java.util.Set;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 import org.apache.log4j.Logger;
-import org.apache.rocketmq.client.consumer.DefaultMQPullConsumer;
-import org.apache.rocketmq.client.consumer.MessageQueueListener;
-import org.apache.rocketmq.client.consumer.PullResult;
-import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
-import org.apache.rocketmq.common.message.MessageExt;
-import org.apache.rocketmq.common.message.MessageQueue;
 
 public class Consumer {
 	private static Logger log = Logger.getLogger(Consumer.class);
-	static  DefaultMQPullConsumer consumer ; 
-	public static void init() throws MQClientException{
-		consumer= new DefaultMQPullConsumer("doubanConsumer");
-		consumer.setNamesrvAddr("176.122.148.51:9876");
-		consumer.setInstanceName("Consumer");
-		consumer.registerMessageQueueListener("douban", null);
-		consumer.start();
+	public static void init()  {
 		
 	}
 	public static void end(){
-		consumer.shutdown();
+		
 	}
 	public static void getMessage(String topic ) throws Exception{
-		Set<MessageQueue> mqs = consumer.fetchSubscribeMessageQueues(topic);
-//	    log.info("mqs.size() " + mqs.size());
 		
-	    for (MessageQueue mq : mqs) {
-	    	System.out.println("-------");
-//	    	log.info("Consume message from queue: " + mq );
-	    	
-	         int cnter = 0;
-	         //每个队列里无限循环，分批拉取未消费的消息，直到拉取不到新消息为止
-	         while (cnter++ < 1) {
-	             long offset = consumer.fetchConsumeOffset(mq, true);
-	             offset = offset < 0 ? 0 : offset;
-//	             log.info("消费进度 Offset: " + offset);
-	             PullResult result = consumer.pull(mq, null, offset,1);
-//	             log.info("接收到的消息集合" + result);
-	             
-	             switch (result.getPullStatus()) {
-		             case FOUND:
-		                 if (result.getMsgFoundList() != null) {
-		                     int prSize = result.getMsgFoundList().size();
-		                     if (prSize != 0) {
-		                         for (MessageExt me : result.getMsgFoundList()) {
-		                             // 消费每条消息，如果消费失败，比如更新数据库失败，就重新再拉一次消息
-		                        	 String messageTopic = me.getTopic();
-		                        	 String messageTag = me.getTags();
-		                        	 byte[] body = me.getBody();
-		                        	 try {
-		                        		 Object objectByByteArray = DataFormat.getObjectByByteArray(body);
-			                        	 log.info("getObject"+objectByByteArray.getClass()+",topic:"+messageTopic+",tag:"+messageTag+",queueid:"+mq.getQueueId()+",offset:"+offset);
-		                        	 } catch (Exception e) {
-		                        		 log.info("body"+new String(body,"utf-8")+",topic:"+messageTopic+",tag:"+messageTag+",queueid:"+mq.getQueueId()+",offset:"+offset);
-		                        	 }
-		                         }
-		                     }
-		                 }
-		                 // 获取下一个下标位置
-		                 offset = result.getNextBeginOffset();
-		                 // 消费完后，更新消费进度
-		                 consumer.updateConsumeOffset(mq, offset);
-		                 break;
-		             case NO_MATCHED_MSG:
-		                 System.out.println("没有匹配的消息");
-		                 break;
-		             case NO_NEW_MSG:
-		                 System.out.println("没有未消费的新消息");
-		                 //拉取不到新消息，跳出 SINGLE_MQ 当前队列循环，开始下一队列循环。
-		                 break ;
-		             case OFFSET_ILLEGAL:
-		                 System.out.println("下标错误");
-		                 break;
-		             default:
-		                 break;
-	             }
-	         }
-	     }
 	}
 	public static void main(String[] args) throws Exception {
-		init();
-		for (int i = 0; i < 10; i++) {
-			Thread.sleep(5000);
-			getMessage("douban");
-		}
-		end();
+		
+        Socket client = new Socket("127.0.0.1", 20006);  
+        client.setSoTimeout(10000); // 读取超时  
+        BufferedOutputStream bos = new BufferedOutputStream(client.getOutputStream());
+        BufferedInputStream bis = new BufferedInputStream(client.getInputStream());  
+        boolean flag = true;  
+        try{ 
+        /*	byte[] buff = new byte[1024];
+        	int read = bis.read(buff);
+        	System.out.println(new String(buff));*/
+            bos.write(("中").getBytes());
+            bos.flush();
+        }catch(SocketTimeoutException e){  
+            System.out.println("Time out, No response");  
+        }  
+        if(client != null){  
+            //如果构造函数建立起了连接，则关闭套接字，如果没有建立起连接，自然不用关闭  
+            client.close(); //只关闭socket，其关联的输入输出流也会被关闭  
+        }
 	}
 }
--- src/arthur/douban/queue/mq/Producer.java
+++ src/arthur/douban/queue/mq/Producer.java
@@ -1,77 +1,75 @@
 package arthur.douban.queue.mq;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.apache.rocketmq.client.exception.MQBrokerException;
-import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.client.producer.DefaultMQProducer;
-import org.apache.rocketmq.client.producer.MessageQueueSelector;
-import org.apache.rocketmq.client.producer.SendResult;
-import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.common.message.MessageQueue;
-import org.apache.rocketmq.remoting.common.RemotingHelper;
-import org.apache.rocketmq.remoting.exception.RemotingException;
-
-import com.mysql.fabric.xmlrpc.base.Array;
-
-import arthur.douban.entity.Group;
-import arthur.douban.entity.Topic;
-import arthur.douban.event.GroupEvent;
-import arthur.douban.event.TopicEvent;
 
 public class Producer {
 	private static Logger log = Logger.getLogger(Producer.class);
-	private static DefaultMQProducer producer ;
-	public static void init() throws MQClientException{
-		producer = new DefaultMQProducer("doubanProducer");
-		producer.setNamesrvAddr("176.122.148.51:9876");
-		producer.setInstanceName("Producer");
-		producer.start();
+	public static void init() throws Exception {
+        ServerSocket server = new ServerSocket(20006);  
+        Socket client = null;  
+        boolean f = true;  
+        while(f){  
+            client = server.accept();  
+            System.out.println("get an client");  
+            new Thread(new ServerThread(client)).start();  
+        }  
+        server.close();  
 	}
 	public static void end(){
-		producer.shutdown();
-	}
-	public static void sendMessage(String topic , String tag , byte[] messge) throws MQClientException, RemotingException, MQBrokerException, InterruptedException{
-		 Message msg = new Message(topic, tag ,
-                 messge);
-         SendResult sendResult = producer.send(msg);
-         log.info(sendResult);
-	}
-	public static void sendMessage(String topic , String tag , byte[][] messages) throws MQClientException, RemotingException, MQBrokerException, InterruptedException{
-		List<Message> list = new ArrayList<Message>();
-		for(int i = 0 ; i< messages.length ;i++){
-			byte[] one = messages[i];
-			Message message = new Message(topic, tag, one);
-			list.add(message);
-		}
-        SendResult sendResult = producer.send(list);
-        log.info(sendResult);
-	}
-	public static void sendMessage(List<Message> list) throws MQClientException, RemotingException, MQBrokerException, InterruptedException{
-        SendResult sendResult = producer.send(list);
-        log.info(sendResult);
+		
 	}
 	public static void main(String[] args) throws Exception {
 		init();
-		List<Message> listG = new ArrayList<Message>();
-		List<Message> listT = new ArrayList<Message>();
-		for(int i = 0 ; i< 20; i++){
-			if(i%2 == 0){
-				GroupEvent groupEvent = new GroupEvent(0, new Group("1321", "1",	 "www.baidu.com"+i, 0), 0);
-				byte[] byteArray = DataFormat.getByteArray(groupEvent);
-				sendMessage("douban", "group", byteArray);
-			}else{
-				Topic topic = new Topic();
-				topic.setTitle("title"+i);
-				TopicEvent te = new TopicEvent(topic);
-				byte[] byteArray = DataFormat.getByteArray(te);
-				sendMessage("douban", "topic", byteArray);
-			}
-		}
-//		sendMessage(listG);
-//		sendMessage(listT);
-		end();
 	}
 }
+class ServerThread implements Runnable {  
+
+    private Socket client = null;
+    private long  heartbeat = 0l;
+    public ServerThread(Socket client){  
+        this.client = client;  
+    }
+    public void test(){
+    	
+    }
+    @Override  
+    public void run() {  
+        try{  
+        	//输出
+        	BufferedOutputStream bos = new BufferedOutputStream(client.getOutputStream());  
+            
+        	//输入
+        	BufferedInputStream bis = new BufferedInputStream(client.getInputStream());  
+            boolean flag =true;  
+            while(flag){  
+            	byte[] buff = new byte[1024];
+            	int read = bis.read(buff);
+            	for(int i = 0 ; i<read ; i++){
+            		System.out.println(buff[i]);
+            	}
+            	
+            	try {
+    				Thread.sleep(3000);
+    			} catch (Exception e) {
+    				// TODO: handle exception
+    			}
+                bos.write("123".getBytes());
+                bos.flush();
+                System.out.println(123);
+            	
+            	
+            }
+            
+            
+        }catch(Exception e){  
+            e.printStackTrace();  
+        }  
+    }  
+
+}  
